4e9102bac8b32d3303180b426ad7f4cd8a1e9861
sophiemassola <sophiemassola.sm@gmail.com>
2022-03-03 01:46:55 -0300

    Entregable2

--- js.js
+++ js.js
@@ -1,26 +1,39 @@
 console.log('Array con objetos');
 
-class Grade {
-    constructor(nombre, nota){
-        this.name = nombre;
-        this.score = nota;
+
+// Objeto ↓↓
+class Ganancias {
+    constructor(a,b){
+        this.day = a;
+        this.income = b;
     }
 }
 
-const subjects = ['Matemática', 'Física', 'Química', 'Arte'];
-const grades = [];
+//Función ↓↓
+const division = (a,b) => a/b;
+const ganancianeta = a => a*0.33;
 
+//Arrays ↓↓
+const week = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes'];
+const incomes = [];
+
+//Variables ↓↓
 let contador = 0;
 let i = 0;
 
-for (const x of subjects){ //acá recorremos todo el array de subjects. La x representa las distintas materias.
-    let score = parseInt(prompt(`Calificación de ${x}`));
-    grades.push(new Grade(x,score)); //acá estoy haciendo un array de objetos de tipo Grade.
-    contador = contador + score;
+//Ciclo For ↓↓
+for (const x of week){ //acá recorremos todo el array de subjects. La x representa los distintos dias de la semana.
+    let ingreso = parseInt(prompt(`Ingresos del día ${x}`));
+    incomes.push(new Ganancias(x,ingreso)); //acá estoy haciendo un array de objetos de tipo Ganancia.
+    contador = contador + ingreso;
     i=i+1;
 }
 
-console.log(grades);
-const division = (a,b) => a/b;
+console.log(incomes);
+
+//Llamado de funciones y display de resultados ↓↓
 let promedio = division (contador,i);
-alert(`El promedio del alumno es ${promedio}`);
+let earnings = ganancianeta(promedio);
+alert(`El promedio de los ingresos de la semana es ${promedio}, de los cuales la ganancia neta de su negocio es ${earnings}`);
+
+
